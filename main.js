b73fb77d74fb8b1f3bcfd17c2ea66317be6c5bee
github9088106153 <87702995+github9088106153@users.noreply.github.com>
2023-05-06 11:54:34 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
+video= "";
+objectDetector="";
+
+function preload(){
+    
+}
 function setup() {
-    canvas = createCanvas(720, 350);
-    canvas.center();
-    video = createCapture(VIDEO);
-    video.size(720,350);
-    video.hide();
-  }
+  canvas = createCanvas(380, 380);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(380,380);
+  video.hide();
+}
+function draw(){
+image(video,0,0,380,380);
+}
+
+function start(){
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML= "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model loaded!")
+    status=true;
+
+}
